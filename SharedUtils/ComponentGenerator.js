49fc77d6de3bedd6489cb3aa1890b0da56e9de79
painforever <bjslxxx@126.com>
2021-11-23 21:19:59 -0500

    a

--- SharedUtils/ComponentGenerator.js
+++ SharedUtils/ComponentGenerator.js
@@ -2,7 +2,52 @@ module.exports = {
     createNativeBaseInput: function (variant='outline', placeholder='Placeholder') {
         return `<Input variant="${variant}" placeholder="${placeholder}" />`
     },
-    createNativeBaseButton: function (key, size = 'md', title='button') {
+    createNativeBaseDateTimeInput: function(placeholder, buttonTitle, mode, stateDate, stateOpen) {
+        return `
+            <HStack space={3} alignItems="center">
+                <Input variant="outline" placeholder="${placeholder}" value={this.state.${stateDate}.toString()} />
+                <Button key={'md'} size={'md'} onPress={ () => this.setState({ ${stateOpen}: true }) }>
+                    ${buttonTitle}
+                </Button>
+                <DatePicker
+                    modal
+                    mode='${mode}'
+                    open={this.state.${stateOpen}}
+                    date={this.state.${stateDate}}
+                    onConfirm={(date) => {
+                      this.setState({ ${stateDate}: date, ${stateOpen}: false });
+                    }}
+                    onCancel={() => {
+                      this.setState({ ${stateOpen}: false });
+                    }}
+                />
+            </HStack>
+        `
+    },
+    createNativeBaseRadioInput: function (title, items, state) {
+        //options should be like: 
+        //[{label: 'Male', value: 'M'}, {label: 'Female', value: 'F'}, ......]
+        return `
+            <HStack space={3} alignItems="center">
+                <Text>${title}: </Text>
+                <Radio.Group
+                    name="${title}"
+                    accessibilityLabel="favorite number"
+                    value={this.state.${state}}
+                    onChange={(nextValue) => { this.setState({ ${state}: nextValue })}}
+                >
+                    <HStack space={3} alignItems="center">
+                        ${items.map(item => {
+                            return `<Radio value="${item.value}" my={1}>
+                               ${item.label}
+                            </Radio>`
+                         })}
+                    </HStack>
+                </Radio.Group>
+            </HStack>
+        `
+    },
+    createNativeBaseButton: function (key, size='md', title='button') {
         if (key === null) key = size
         return `<Button key={'${key}'} size={'${size}'}>
                     ${title}
--- extension.js
+++ extension.js
@@ -1,11 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-const fs = require('fs');
-const path = require('path');
-const StatesExtractor = require('./SharedUtils/StatesExtractor');
-const FileFolderUtils = require('./SharedUtils/FileFolderUtils');
-const ConstructModule = require('./ModuleTemplate/ConstructModule');
+const vscode = require("vscode");
+const fs = require("fs");
+const path = require("path");
+const StatesExtractor = require("./SharedUtils/StatesExtractor");
+const FileFolderUtils = require("./SharedUtils/FileFolderUtils");
+const ConstructModule = require("./ModuleTemplate/ConstructModule");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,19 +14,23 @@ const ConstructModule = require('./ModuleTemplate/ConstructModule');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('Congratulations, your extension "hayai" is now active!');
-	//1, insert a code snipt by string and create a file for it.
-	let disposable = vscode.commands.registerCommand('hayai.createBoilerplate', async function () {
-		// The code you place here will be executed every time your command is executed
-		if (!vscode.workspace) {
-			return vscode.window.showErrorMessage('Please open a project folder first');
-		  }
-	  
-		  const folderPath = vscode.workspace.workspaceFolders[0].uri
-			.toString()
-			.split(':')[1];
-	  
-		  const htmlContent = `<!DOCTYPE html>
+  console.log('Congratulations, your extension "hayai" is now active!');
+  //1, insert a code snipt by string and create a file for it.
+  let disposable = vscode.commands.registerCommand(
+    "hayai.createBoilerplate",
+    async function () {
+      // The code you place here will be executed every time your command is executed
+      if (!vscode.workspace) {
+        return vscode.window.showErrorMessage(
+          "Please open a project folder first"
+        );
+      }
+
+      const folderPath = vscode.workspace.workspaceFolders[0].uri
+        .toString()
+        .split(":")[1];
+
+      const htmlContent = `<!DOCTYPE html>
 	  <html lang="en">
 	  <head>
 		<meta charset="UTF-8" />
@@ -39,79 +43,100 @@ function activate(context) {
 		<script src="app.js"></script>
 	  </body>
 	  </html>`;
-	  
-		fs.writeFile(path.join(folderPath, 'index.html'), htmlContent, (err) => {
-		if (err) {
-			return vscode.window.showErrorMessage(
-			'Failed to create boilerplate file!'
-			);
-		}
-		  vscode.window.showInformationMessage('Created boilerplate files');
-		});
-	});
 
+      fs.writeFile(path.join(folderPath, "index.html"), htmlContent, (err) => {
+        if (err) {
+          return vscode.window.showErrorMessage(
+            "Failed to create boilerplate file!"
+          );
+        }
+        vscode.window.showInformationMessage("Created boilerplate files");
+      });
+    }
+  );
 
-	//2, insert a code snipt in an active editor tab for whereever your cursor is
-	let disposable2 = vscode.commands.registerCommand('hayai.insertBubbleSort', async function() {
-		if (!vscode.workspace) {
-		    return vscode.window.showErrorMessage('Please open a project folder first');
-		}
+  //2, insert a code snipt in an active editor tab for whereever your cursor is
+  let disposable2 = vscode.commands.registerCommand(
+    "hayai.insertBubbleSort",
+    async function () {
+      if (!vscode.workspace) {
+        return vscode.window.showErrorMessage(
+          "Please open a project folder first"
+        );
+      }
 
-		const activeEditor = vscode.window.activeTextEditor
-		if (activeEditor) {
-            let document = activeEditor.document;
-            // Get the document text
-            const documentText = document.getText();
-			activeEditor.edit((selectedText) => {
-				selectedText.replace(activeEditor.selection, "This is my fucking bubble sort!");
-			})
-		}
-	})
+      const activeEditor = vscode.window.activeTextEditor;
+      if (activeEditor) {
+        let document = activeEditor.document;
+        // Get the document text
+        const documentText = document.getText();
+        activeEditor.edit((selectedText) => {
+          selectedText.replace(
+            activeEditor.selection,
+            "This is my fucking bubble sort!"
+          );
+        });
+      }
+    }
+  );
 
+  //3, open a dialog and input whatever you want, and insert a code snipt by importin a node module
+  let disposable3 = vscode.commands.registerCommand(
+    "hayai.createModule",
+    async function () {
+      if (!vscode.workspace) {
+        return vscode.window.showErrorMessage(
+          "Please open a project folder first"
+        );
+      }
 
-	//3, open a dialog and input whatever you want, and insert a code snipt by importin a node module
-	let disposable3 = vscode.commands.registerCommand('hayai.createModule', async function() {
-		if (!vscode.workspace) {
-		    return vscode.window.showErrorMessage('Please open a project folder first');
-		}
+      const workspaceFolders = vscode.workspace.workspaceFolders[0].uri
+        .toString()
+        .split(":")[1];
 
-		const workspaceFolders = vscode.workspace.workspaceFolders[0].uri.toString().split(':')[1];
+      const input = await vscode.window.showInputBox({
+        placeHolder: "Put the class name and its states here...",
+        prompt: "Put the class name and its states here...",
+        value: "",
+      });
 
-		const input = await vscode.window.showInputBox({
-			placeHolder: "Put the class name and its states here...",
-			prompt: "Put the class name and its states here...",
-			value: ''
-		});
+      if (!input || input.trim().length == 0) {
+        vscode.window.showErrorMessage(
+          "Please input something to make it work :)"
+        );
+        return;
+      }
 
-		if (!input || input.trim().length == 0) {
-			vscode.window.showErrorMessage('Please input something to make it work :)')
-			return
-		}
+      const data = await StatesExtractor.extractStates(input);
+      const json = JSON.parse(data.choices[0].text.trim());
+      console.log(json);
+      const className = json.className;
+      const states = json.states;
+      ConstructModule.assemblyLine(workspaceFolders, className, states);
+    }
+  );
 
-		const data = await StatesExtractor.extractStates(input);
-		const json = JSON.parse(data.choices[0].text.trim());
-		console.log(json);
-		const className = json.className
-		const states = json.states
-		ConstructModule.assemblyLine(workspaceFolders, className, states)
-	});
+  let disposable4 = vscode.commands.registerCommand(
+    "hayai.test",
+    async function () {
+      FileFolderUtils.getFolders();
+      const terminal = vscode.window.createTerminal(`yusong`);
+	  terminal.sendText("ls");
+	  terminal.sendText("date");
+      terminal.show();
+    }
+  );
 
-	let disposable4 = vscode.commands.registerCommand('hayai.test', async function () {
-		FileFolderUtils.getFolders();
-	});
-
-
-
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(disposable2);
-	context.subscriptions.push(disposable3);
-	context.subscriptions.push(disposable4);
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable2);
+  context.subscriptions.push(disposable3);
+  context.subscriptions.push(disposable4);
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
