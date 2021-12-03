ddec721af1bb925395e4a634e0de0eb4adb857a1
painforever <bjslxxx@126.com>
2021-12-02 21:21:01 -0500

    a

--- extension.js
+++ extension.js
@@ -3,6 +3,8 @@
 const vscode = require("vscode");
 const fs = require("fs");
 const path = require("path");
+
+//Modules for creating modules
 const StatesExtractor = require("./SharedUtils/StatesExtractor");
 const FileFolderUtils = require("./SharedUtils/FileFolderUtils");
 const ConstructModule = require("./ModuleTemplate/ConstructModule");
@@ -11,8 +13,8 @@ const ConstructModule = require("./ModuleTemplate/ConstructModule");
 const CommandsNeedToRun = require('./ProjectInitializer/CommandsNeedToRun')
 const FileFoldersGenerator = require('./ProjectInitializer/FileFoldersGenerator')
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+//Modules for Drawer
+
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -71,34 +73,7 @@ function activate(context) {
     }
   );
 
-  //3, open a dialog and input whatever you want, and insert a code snipt by importin a node module
-  let createModuleCommandDisposable = vscode.commands.registerCommand("hayai.createModule", async function () {
-      if (!vscode.workspace) {
-        return vscode.window.showErrorMessage(
-          "Please open a project folder first"
-        );
-      }
-
-      const workspaceFolders = vscode.workspace.workspaceFolders[0].uri.toString().split(":")[1];
-      const input = await vscode.window.showInputBox({
-        placeHolder: "Put the class name and its states here...",
-        prompt: "Put the class name and its states here...",
-        value: "",
-      });
-
-      if (!input || input.trim().length == 0) {
-        vscode.window.showErrorMessage("Please input something to make it work :)");
-        return;
-      }
-      const data = await StatesExtractor.extractStates(input);
-      const json = JSON.parse(data.choices[0].text.trim());
-      console.log(`DEBUG: ${json}`);
-      const className = json.className;
-      const states = json.states;
-      ConstructModule.assemblyLine(workspaceFolders, className, states);
-    }
-  );
-
+  //Create all needed folders and files, this is generally done for the 1st step
   let createSetUpFolderAndFilesDisposable = vscode.commands.registerCommand("hayai.createSetUpFolderAndFiles", async function () {
     const workspaceFolders = vscode.workspace.workspaceFolders[0].uri.toString().split(":")[1];
     const terminal = vscode.window.createTerminal(`hayai`);
@@ -112,6 +87,41 @@ function activate(context) {
     terminal.show()
   });
 
+  //Open a dialog and input whatever you want, and insert a code snipt by importing a node module
+  let createModuleCommandDisposable = vscode.commands.registerCommand("hayai.createModule", async function () {
+    if (!vscode.workspace) {
+      return vscode.window.showErrorMessage("Please open a project folder first");
+    }
+
+    const workspaceFolders = vscode.workspace.workspaceFolders[0].uri.toString().split(":")[1];
+    const input = await vscode.window.showInputBox({
+      placeHolder: "Put the class name and its states here...",
+      prompt: "Put the class name and its states here...",
+      value: "",
+    });
+
+    if (!input || input.trim().length == 0) {
+      vscode.window.showErrorMessage("Please input something to make it work :)");
+      return;
+    }
+    const data = await StatesExtractor.extractStates(input);
+    const json = JSON.parse(data.choices[0].text.trim());
+    console.log(`DEBUG: ${json}`);
+    const className = json.className;
+    const states = json.states;
+    ConstructModule.assemblyLine(workspaceFolders, className, states);
+  }
+  );
+
+  //Construct the whole project modules as Drawer
+  let constructProjectAsDrawer = vscode.commands.registerCommand("hayai.reconstructAsDrawer", async function () {
+    if (!vscode.workspace) {
+      return vscode.window.showErrorMessage("Please open a project folder first");
+    }
+    const workspaceFolders = vscode.workspace.workspaceFolders[0].uri.toString().split(":")[1];
+
+  })
+
   let disposable4 = vscode.commands.registerCommand("hayai.test", async function () {
       FileFolderUtils.getFolders();
       const terminal = vscode.window.createTerminal(`yusong`);
@@ -123,8 +133,8 @@ function activate(context) {
 
   context.subscriptions.push(disposable);
   context.subscriptions.push(disposable2);
-  context.subscriptions.push(createModuleCommandDisposable);
   context.subscriptions.push(createSetUpFolderAndFilesDisposable);
+  context.subscriptions.push(createModuleCommandDisposable);
   context.subscriptions.push(disposable4);
 }
 
