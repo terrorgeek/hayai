6f458553b80eb300d3bb9eb0ea528a6d513c78bb
Yu Song <bjslxxx@126.com>
2023-04-25 20:41:58 -0400

    Delete Constants.js

--- Constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    CodexBaseUrl: "https://api.openai.com/v1/engines/davinci-codex/completions",
-    SearchBaseUrl: "https://api.openai.com/v1/engines/davinci/search",
-    OpenAIAPIKey: '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',
-    SrcFolderName: 'src'
-}
